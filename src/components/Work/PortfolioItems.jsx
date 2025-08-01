f4d32859306ecc48040eb34fe6ad6fb6135a8820
Sebastian Cruz <79765472+SCR08@users.noreply.github.com>
2025-08-01 03:26:47 -0500

    Add alt text for images

--- src/components/Work/PortfolioItems.jsx
+++ src/components/Work/PortfolioItems.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const PortfolioItems = ({item}) => {
   return (
-    <a href={item.url} className="portfolio__card" target="_blank" rel="noopener noreferrer" key={item.id}>
-      <img src={item.image} alt="" className="portfolio__img" />
+      <a href={item.url} className="portfolio__card" target="_blank" rel="noopener noreferrer" key={item.id}>
+        <img src={item.image} alt={item.title} className="portfolio__img" />
       <h3 className="portfolio__title">{item.title}</h3>
       <div className="portfolio__button">
         More Info <i className="bx bx-right-arrow-alt portfolio__button-icon"></i>
