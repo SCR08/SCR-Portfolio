cb17d330397751bca238f5a782594f285941eeee
Sebastian Cruz <79765472+SCR08@users.noreply.github.com>
2025-08-01 03:16:16 -0500

    Add company work section

---
+++ src/components/CompanyWork/Data.jsx
@@ -0,0 +1,42 @@
+import BankHero from '../../assets/BankHero.png';
+import Parche1 from '../../assets/Parche1.png';
+import Solar1 from '../../assets/Solar1.png';
+
+export const companyProjectsData = [
+  {
+    id: 1,
+    image: BankHero,
+    title: 'Website Redesign',
+    category: 'Company A',
+    url: 'https://example.com/company-a-project'
+  },
+  {
+    id: 2,
+    image: Parche1,
+    title: 'Mobile App',
+    category: 'Company B',
+    url: 'https://example.com/company-b-project'
+  },
+  {
+    id: 3,
+    image: Solar1,
+    title: 'Dashboard UI',
+    category: 'Company C',
+    url: 'https://example.com/company-c-project'
+  }
+];
+
+export const companyNav = [
+  {
+    name: 'All'
+  },
+  {
+    name: 'Company A'
+  },
+  {
+    name: 'Company B'
+  },
+  {
+    name: 'Company C'
+  }
+];
