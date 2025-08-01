601d81ab7853cdae7caf288720550beaffb6b9ee
Sebastian Cruz <79765472+SCR08@users.noreply.github.com>
2025-08-01 03:06:38 -0500

    Add meaningful alt text

--- src/components/Work/PortfolioItems.jsx
+++ src/components/Work/PortfolioItems.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PortfolioItems = ({item}) => {
   return (
     <a href={item.url} className="portfolio__card" target="_blank" rel="noopener noreferrer" key={item.id}>
-      <img src={item.image} alt="" className="portfolio__img" />
+      <img src={item.image} alt={`Screenshot of ${item.title} project`} className="portfolio__img" />
       <h3 className="portfolio__title">{item.title}</h3>
       <div className="portfolio__button">
         More Info <i className="bx bx-right-arrow-alt portfolio__button-icon"></i>
