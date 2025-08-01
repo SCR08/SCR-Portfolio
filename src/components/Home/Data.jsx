2090ba0060577b1f5368159d84cef72f3b23387e
Sebastian Cruz <79765472+SCR08@users.noreply.github.com>
2025-08-01 03:27:17 -0500

    Fix JSX class attributes

--- src/components/Home/Data.jsx
+++ src/components/Home/Data.jsx
@@ -9,7 +9,7 @@ const Data = () => {
         <a href="#contact" className="button button--flex">
             Let's talk!
             <svg
-                  class="button__icon"
+                  className="button__icon"
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
                   height="24"
